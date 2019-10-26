48c0aff6bb6a02619ac7ba691fb7ec8fba927b76
mechpig <7295856+mech-pig@users.noreply.github.com>
2019-10-26 23:22:23 +0200

    add: monad transformers module

---
+++ src/MonadTransformers.purs
@@ -0,0 +1,71 @@
+module MonadTransformers where
+
+import Prelude ((<>), ($), (+), (-), bind, discard, show, Unit)
+
+import Control.Monad (class Monad)
+import Control.Monad.Reader.Class (class MonadReader, asks)
+import Control.Monad.Reader.Trans (runReaderT)
+import Control.Monad.State.Class (class MonadState, modify)
+import Control.Monad.State.Trans (evalStateT)
+import Data.List (List(..), (:))
+import Data.Foldable (foldl)
+import Effect (Effect)
+import Effect.Console (log)
+
+
+-- Entities
+data Step = Move Direction Int | Rest
+
+data Direction = Up | Right | Left | Down
+
+type Instructions = List Step
+
+type Point = { x :: Int, y :: Int }
+
+type Path = List Point
+
+move :: Step -> Point -> Point
+move Rest origin = origin
+move (Move Up d)    { x, y } = { x, y: y + d }
+move (Move Right d) { x, y } = { x: x + d, y }
+move (Move Down d)  { x, y } = { x, y: y - d }
+move (Move Left d)  { x, y } = { x: x - d, y }
+
+
+-- State
+type RoverState = { currentPosition :: Point }  -- totalDistance :: Int
+
+updateRoverState :: Step -> RoverState -> RoverState
+updateRoverState step state = {
+  currentPosition: move step state.currentPosition
+}
+
+
+-- Reader
+type RoverConfiguration = { instructions :: Instructions }
+
+
+roverProgram :: forall m.
+                MonadReader RoverConfiguration m =>
+                MonadState RoverState m =>
+                m RoverState
+roverProgram = do
+  instructions <- asks _.instructions
+  modify (\initState -> executeProgram initState instructions)
+  where
+    executeProgram = foldl (\currentState step -> updateRoverState step currentState)
+
+
+runRoverProgram :: forall m. Monad m => RoverConfiguration -> RoverState -> m RoverState
+runRoverProgram roverConfiguration initialState =
+  evalStateT (runReaderT roverProgram roverConfiguration) initialState
+
+
+main :: Effect Unit
+main = do
+  let initialState = { currentPosition: { x: 0, y: 1 } }
+  let configuration = { instructions: ((Move Up 3) : (Move Left 1) : Nil) }
+  log $ "initialState:\n\t" <> show initialState
+  -- TODO(missing show instance for Step): log $ "configuration:\n\t" <> show configuration
+  finalState <- runRoverProgram configuration initialState
+  log $ "finalState:\n\t" <> show finalState
